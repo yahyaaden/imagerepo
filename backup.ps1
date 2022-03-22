bcb87e34137ba0beaaab9492a48ea4d3ef0592a6
unknown <yahya.a.aden@gmail.com>
2022-03-22 14:50:12 +0100

    Cleanup of WD + modified PS script

--- backup.ps1
+++ backup.ps1
@@ -1,3 +1,22 @@
-#backup
-docker exec imagerepo-db-1 /usr/bin/mysqldump -u root --password=root imagerepo | Set-Content backup.sql
-Write-Host "The database has been stored in backup.sql"
+#We take the command input and store it in a variable
+$getarg = $args[0]
+
+#We check if the variable meets the criteria of our conditions:
+
+if ($getarg -eq "backup")
+{
+    #store database in the backup.sql in the backup folder
+    docker exec imagerepo-db-1 /usr/bin/mysqldump -u root --password=root imagerepo | Set-Content ./backup/backup.sql
+    Write-Host "The database has been stored in backup.sql" -ForegroundColor Green
+}
+
+elseif ($getarg -eq "restore")
+{
+    #restore database from backup.sql in the backup folder
+    Get-Content ./backup/backup.sql | docker exec -i imagerepo-db-1 /usr/bin/mysql -u root --password=root imagerepo
+    Write-Host "The database has been restored from backup.sql" -ForegroundColor Blue
+}
+else
+{
+    Write-Host "The command does not meet the conditions of the script!" -ForegroundColor Red
+}
